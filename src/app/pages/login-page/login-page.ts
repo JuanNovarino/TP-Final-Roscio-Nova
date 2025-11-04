348ac404577897356583546934d2a44df40620f0
JuanNovarino <juansnovarino2005@gmail.com>
2025-11-04 17:37:19 -0300

    Logica y Css Login funcionando

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
 import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterModule, FormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
 
+  errorLogin = false;
+  authService = inject(AuthService);
+
+  async login(form:any){
+    console.log(form.value)
+    this.errorLogin = false;
+    if(!form.value.restaurantName || !form.value.password){
+      this.errorLogin = true;
+      return
+    }
+    await this.authService.login(form.value);
+    this.errorLogin = true;
+  }
 }
--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,8 +1,59 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, OnInit } from '@angular/core';
+import { LoginData } from '../interfaces/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService implements OnInit {
+  
+  router = inject(Router);
+  token : null|string = localStorage.getItem("token");
+  revisionTokenInterval: number | undefined;
+
+  ngOnInit(): void {
+    
+    if(this.token){
+      this.revisionTokenInterval = this.revisionToken() //el window soluciona la posibilidad de que reivision interval sea un timeout
+    }
+  }
+
+  async login(loginData:LoginData ){
+    const res = await fetch("https://w370351.ferozo.com/api/Authentication/login",
+      {
+        method:"POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(loginData)
+      }
+    )
+    if(res.ok){
+      this.token = await res.text()
+      localStorage.setItem("token",this.token);
+      this.router.navigate(["/"])
+    }
+  }
+
+  logout(){
+    this.token = null;
+    localStorage.removeItem("token");
+    this.router.navigate(["/login"])
+  }
+
+  revisionToken() {
+    return window.setInterval(() => { //le agregue el window.setinterval ya que con este nos aseguramos que sea un number debido a que restringimos el enfasis al navegador
+      if (this.token) {
+        const base64Url = this.token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        const claims: { exp: number } = JSON.parse(jsonPayload);
+        if (new Date(claims.exp * 1000) < new Date()) {
+          this.logout()
+        }
+      }
+    }, 600)
+  }
   
 }
