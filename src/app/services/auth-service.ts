7ce7808b80485ddd2795c1af292d0b84239b5b87
JuanNovarino <juansnovarino2005@gmail.com>
2025-11-08 23:41:05 -0300

    Product Service (sin completar)

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -33,6 +33,22 @@ export class AuthService implements OnInit {
     }
   }
 
+    parseJwt (token : string) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+    }
+
+    getUserId() {
+    if (!this.token) return;
+    return parseInt(this.parseJwt(this.token).sub);
+    }
+
+
   logout(){
     this.token = null;
     localStorage.removeItem("token");
