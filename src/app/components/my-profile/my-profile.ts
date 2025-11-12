32f0f9036a66ec767a295786e72b891fd49f4597
JuanNovarino <juansnovarino2005@gmail.com>
2025-11-12 19:22:56 -0300

    Resolucion de ZoneLess, Resolucion Auth-Service, Error SSR

--- src/app/components/my-profile/my-profile.ts
+++ src/app/components/my-profile/my-profile.ts
@@ -7,59 +7,54 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-my-profile',
-  imports: [RouterModule,FormsModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './my-profile.html',
   styleUrl: './my-profile.scss',
-  standalone : true // ??
+  standalone: true // ??
 })
 export class MyProfile implements OnInit {
 
 
-   userService = inject(UserService);
-   authService = inject(AuthService);
-   router = inject(Router);
-  
-   
+  userService = inject(UserService);
+  authService = inject(AuthService);
+  router = inject(Router);
 
-  
+
+
+
   userProfile: User | undefined;
   //userEditado: User | undefined; 
   cargandoPerfil: boolean = false;
   //isEditing: boolean = false;
 
-  
-  async ngOnInit() {
 
-     this.cargandoPerfil = true;
-    
-    const loggedUserId = this.authService.getUserId(); 
-    
+  async ngOnInit() {    
+    const loggedUserId = this.authService.getUserId();
+    console.log(loggedUserId);
     if (loggedUserId) {
-      
-      const res = await this.userService.getUserbyid(loggedUserId); 
+      this.cargandoPerfil = true;
+      const res = await this.userService.getUserbyid(loggedUserId);
+      this.cargandoPerfil = false;
       this.userProfile = res;
-      this.cargandoPerfil = false; 
-      
-    } 
-    
+    }
   }
 
-  async deleteUser(){
-    if(this.userProfile){
+  async deleteUser() {
+    if (this.userProfile) {
 
-       //if (!confirm(`¿Estás seguro de que quieres eliminar la cuenta? Esta acción es irreversible.`)) {
-         //   return;
-       // }
+      //if (!confirm(`¿Estás seguro de que quieres eliminar la cuenta? Esta acción es irreversible.`)) {
+      //   return;
+      // }
 
       const res = await this.userService.deleteUser(this.userProfile.id);
       //console.log("ID de usuario a eliminar:", this.userProfile.id);
-      if(res) {
-            alert('Cuenta eliminada con éxito.');
-            this.authService.logout(); 
-            this.router.navigate(['/']); 
-        } else {
-            alert('Error al eliminar la cuenta.');
-        }
+      if (res) {
+        alert('Cuenta eliminada con éxito.');
+        this.authService.logout();
+        this.router.navigate(['/']);
+      } else {
+        alert('Error al eliminar la cuenta.');
+      }
       //if(res) this.router.navigate(['/login']);
     }
   }
--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,63 +1,55 @@
-import { Injectable, inject, OnInit } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { LoginData } from '../interfaces/auth';
 import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService implements OnInit {
-  
+export class AuthService{
+
   router = inject(Router);
-  token : null|string = null; //Se renderiza
+  token : null|string = null;
   revisionTokenInterval: number | undefined;
-  id : number | undefined = undefined ;
+  id: number | undefined = undefined;
 
-  ngOnInit(): void {
-    
-    if(this.token){
-      
-    }
-  }
+  
 
-  async login(loginData:LoginData ){
+  async login(loginData: LoginData) {
     const res = await fetch("https://w370351.ferozo.com/api/Authentication/login",
       {
-        method:"POST",
-        headers: {'Content-Type': 'application/json'},
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(loginData)
       }
     )
-    if(res.ok){
+    if (res.ok) {
       const obj = await res.json()
       this.token = obj.token;
-      localStorage.setItem("token",this.token!);
-      this.id =this.getUserId();
+      localStorage.setItem("token", this.token!);
+      this.id = this.getUserId();
       this.router.navigate(["/admin"])
     }
   }
 
-    parseJwt (token : string) {
+  parseJwt(token: string) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
 
     return JSON.parse(jsonPayload);
-    }
+  }
 
-    getUserId() {
+  getUserId() {
+    this.token = localStorage.getItem("token"); //
     if (!this.token) return;
     return parseInt(this.parseJwt(this.token).sub);
-    }
+  }
 
-
-  logout(){
+  logout() {
     this.token = null;
     localStorage.removeItem("token");
     this.router.navigate(["/"])
   }
-
-  
-  
 }
