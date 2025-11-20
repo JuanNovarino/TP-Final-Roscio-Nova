604e4293ca454905579ec195e09f359c98c7aaa5
JuanNovarino <juansnovarino2005@gmail.com>
2025-11-19 23:40:35 -0300

    Muestreo de Categorias Funcionando

---
+++ src/app/component/category-list/category-list.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-category-list',
+  imports: [],
+  templateUrl: './category-list.html',
+  styleUrl: './category-list.scss',
+})
+export class CategoryList {
+
+ 
+
+}
--- src/app/pages/menu/menu.ts
+++ src/app/pages/menu/menu.ts
@@ -1,20 +1,57 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category-service';
 import { AuthService } from '../../services/auth-service';
 import { Category } from '../../interfaces/category';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './menu.html',
-  styleUrl: './menu.scss',
+   
 })
-export class Menu {
-
-  categoriesService = inject(CategoryService)
+export class Menu implements OnInit {
+categoriesService = inject(CategoryService)
   authService = inject(AuthService)
-  id = input.required<number>()
+  route = inject(ActivatedRoute); 
+
+  categoryList : Category[] | undefined
+  loadingCategories = true;
+
+  async ngOnInit() {
+    
+    this.loadingCategories = true;
+
+    const idUserString = this.route.snapshot.paramMap.get('idUser');
+
+    
+    if (!idUserString) {
+      this.loadingCategories = false;
+      return; 
+    }
+
+    const restaurantid = parseInt(idUserString, 10);
+
+    
+    const categoriesData = await this.categoriesService.getCategoriesByUserId(restaurantid);
+
+    if (categoriesData) {
+        this.categoryList = categoriesData;
+    }
+    
+
+    this.loadingCategories = false;
+  }
+  
+  }
+
+  
+ 
+
+/*categoriesService = inject(CategoryService)
+  authService = inject(AuthService)
+  IdUser = input.required<number>()
   categoryList : Category[] | undefined
 
   loadingCategories = true;
@@ -22,7 +59,7 @@ export class Menu {
    async ngOnInit() {
     this.loadingCategories = true;
 
-    const restaurantid = this.id()
+    const restaurantid = this.IdUser()
 
     const categoriesData = await this.categoriesService.getCategoriesByUserId(restaurantid)
 
@@ -32,7 +69,7 @@ export class Menu {
     console.log(this.categoryList)
 
     this.loadingCategories = false;
+   }*/
 
-  }
+  
 
-}
