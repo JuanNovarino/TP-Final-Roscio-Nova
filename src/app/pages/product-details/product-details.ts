3e35ac173166698240471dcb8a85f093813d1532
TomasRosciolino <tomasrosciolino@gmail.com>
2025-11-24 19:55:45 -0300

    muestreo de product details

--- src/app/pages/product-details/product-details.ts
+++ src/app/pages/product-details/product-details.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,input,inject } from '@angular/core';
+import { Product } from '../../interfaces/product';
+import { ProductService } from '../../services/product-service';
+import { Router,RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -8,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class ProductDetails {
 
+  IdProduct = input.required<string>()
+  productService = inject(ProductService)
+  producto : Product | undefined;
+  router = inject(Router);
+  cargandoProducto = false
+
+   async ngOnInit() {
+    if(this.IdProduct()){
+      // Si encuentro el contacto en el array del servicio lo uso, mientras tanto cargo el contacto del backend por si hubo cambios en el contacto
+      this.producto = this.productService.products.find(producto => producto.id.toString() === this.IdProduct());
+      if(!this.producto) this.cargandoProducto = true;
+      const res = await this.productService.getProductById(this.IdProduct());
+      if(res) this.producto = res;
+      this.cargandoProducto = false;
+    }
+  }
 }
+
