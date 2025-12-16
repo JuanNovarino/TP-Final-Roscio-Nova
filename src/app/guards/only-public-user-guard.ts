147550e983913e0efd91726893605e47b0857940
JuanNovarino <juansnovarino2005@gmail.com>
2025-12-16 14:46:06 -0300

    Guards Funcionando

--- src/app/guards/only-public-user-guard.ts
+++ src/app/guards/only-public-user-guard.ts
@@ -1,17 +1,17 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, RedirectCommand, Router } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router'; 
 import { AuthService } from '../services/auth-service';
 
 
 export const onlyPublicUserGuard: CanActivateFn = (route, state) => {
   const auth = inject(AuthService);
   const router = inject(Router);
-  if (auth.token) {
-    console.log('Guardia: Usuario logeado, redirigiendo a /home');
-    const newPath = router.parseUrl(""); 
-    return new RedirectCommand(newPath, {
-      skipLocationChange: true,
-    });
+
+  if (auth.getToken()) { 
+    console.log('Guardia: Usuario logeado, redirigiendo a /admin');
+    return router.parseUrl("/admin"); 
   }
+  
+  
   return true;
 };
